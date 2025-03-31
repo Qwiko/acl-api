65769d8741a513bc548c1af8e9a1c8f48a95c2c9
Qwiko <jakob.mellberg@gmail.com>
2025-03-31 17:52:08 +0000

    Fixed require_unique function

--- src/app/schemas/custom_validators.py
+++ src/app/schemas/custom_validators.py
@@ -3,8 +3,9 @@ from pydantic import AfterValidator
 
 def require_unique(v):
     """
-    Check that the list is unique."""
-    if v != set(v):
+    Check that the list is unique.
+    """
+    if len(v) != len(set(v)):
         raise ValueError("not unique")
     return v
 
